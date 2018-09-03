38a48408cf25d46bb7fefd8440d235e4aae7f4db
Josh Matthews <josh@joshmatthews.net>
2018-09-03 15:12:49 -0400

    Add insanifest and ruin driver 3 to test suite.

--- test.sh
+++ test.sh
@@ -7,3 +7,5 @@ cargo run ~/mzx/sr/SPIRTREV.MZX 0 out.png
 cargo run ~/mzx/btb/BERNARD.MZX 0 out.png
 cargo run ~/mzx/caverns/CAVERNS.MZX 0 out.png
 cargo run ~/mzx/DE_Game/DE_MAIN.MZX 0 out.png
+cargo run ~/mzx/rd3TSE/rd3TSE.mzx 0 out.png
+cargo run ~/mzx/insanifs/INSANIFS.mzx 0 out.png
