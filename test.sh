a3f106b459b6f3218332524cd05b566e9274aa7a
Josh Matthews <josh@joshmatthews.net>
2018-09-03 16:12:50 -0400

    Refine test harness.

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@ set -o nounset
 set -o pipefail
 
 export RUST_BACKTRACE=1
-cargo run ~/mzx/sr/SPIRTREV.MZX 0 out.png
-cargo run ~/mzx/btb/BERNARD.MZX 0 out.png
-cargo run ~/mzx/caverns/CAVERNS.MZX 0 out.png
-cargo run ~/mzx/DE_Game/DE_MAIN.MZX 0 out.png
-cargo run ~/mzx/rd3TSE/rd3TSE.mzx 0 out.png
-cargo run ~/mzx/insanifs/INSANIFS.mzx 0 out.png
+cargo build
+TESTS=(sr/SPIRTREV.MZX btb/BERNARD.MZX caverns/CAVERNS.mzx DE_Game/DE_MAIN.MZX rd3TSE/rd3TSE.mzx insanifs/INSANIFS.MZX)
+for test in ${TESTS[@]}; do
+    echo Testing $test.
+    target/debug/mzxview ~/mzx/${test} 0 out.png
+    echo Passed.
+done
