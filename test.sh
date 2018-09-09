1c2d279ff5f4aa04457ffac64f89de4c31782566
Josh Matthews <josh@joshmatthews.net>
2018-09-09 12:56:52 -0400

    Add Celestial Altar and Smiley RPG to testsuite.

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 export RUST_BACKTRACE=1
 cargo build
-TESTS=(sr/SPIRTREV.MZX btb/BERNARD.MZX caverns/CAVERNS.mzx DE_Game/DE_MAIN.MZX rd3TSE/rd3TSE.mzx insanifs/INSANIFS.MZX)
+TESTS=(sr/SPIRTREV.MZX btb/BERNARD.MZX caverns/CAVERNS.mzx DE_Game/DE_MAIN.MZX rd3TSE/rd3TSE.mzx insanifs/INSANIFS.MZX 30641/30641.mzx srpgmain/SMILYRPG.MZX)
 for test in ${TESTS[@]}; do
     echo Testing $test.
     target/debug/mzxview ~/mzx/${test} 0 out.png
