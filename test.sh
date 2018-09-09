c4d931a5b389aeceb71d946fee1c8ff9bd0755e2
Josh Matthews <josh@joshmatthews.net>
2018-09-09 13:03:28 -0400

    Add remaining Zeux titles to test suite.

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 export RUST_BACKTRACE=1
 cargo build
-TESTS=(sr/SPIRTREV.MZX btb/BERNARD.MZX caverns/CAVERNS.mzx DE_Game/DE_MAIN.MZX rd3TSE/rd3TSE.mzx insanifs/INSANIFS.MZX 30641/30641.mzx srpgmain/SMILYRPG.MZX)
+TESTS=(sr/SPIRTREV.MZX btb/BERNARD.MZX caverns/CAVERNS.mzx DE_Game/DE_MAIN.MZX rd3TSE/rd3TSE.mzx insanifs/INSANIFS.MZX 30641/30641.mzx srpgmain/SMILYRPG.MZX chronos/CHRONOS.MZX forest/FOREST.MZX catacomb/CATACOMB.MZX)
 for test in ${TESTS[@]}; do
     echo Testing $test.
     target/debug/mzxview ~/mzx/${test} 0 out.png
